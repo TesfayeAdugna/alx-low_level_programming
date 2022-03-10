344f4876a665dbf3002b0fcf79be6313145a7173
root <adugnatesfaye888@gmail.com>
2022-03-10 12:27:08 -0800

    C program that prints Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+   * main - print to screen
+   *
+   * Return: zero
+   */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
+}
