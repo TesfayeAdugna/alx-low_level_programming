a9715110e7f9d95c08b20c01c5a4ac2f46af7991
root <adugnatesfaye888@gmail.com>
2022-03-22 14:21:07 -0700

    betty description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,9 @@
 #include "main.h"
 #include <stdio.h>
+/*
+ * this is a function that resets the value of n from 402 to 98
+ * it doesn't return anything
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
