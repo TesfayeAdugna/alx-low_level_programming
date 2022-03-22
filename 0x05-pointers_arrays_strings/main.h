6a28b3dfea93ac4a941501e77bb06d0a9d9fcf5e
root <adugnatesfaye888@gmail.com>
2022-03-22 14:03:17 -0700

    adding main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
+void rev_string(char *s);
+int string_length(char*);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
