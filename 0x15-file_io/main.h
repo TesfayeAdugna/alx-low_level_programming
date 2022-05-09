cd55ebec502c573d34791d288987f1d2b27fdca6
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-05-09 12:03:06 +0300

    file input output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
